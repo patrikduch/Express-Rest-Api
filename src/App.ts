a8fca1fe1281f73f90eda50311618ae9256a41d3
patrikduch <duchpatrik@icloud.com>
2020-10-11 16:57:47 +0200

    PORT fix

--- src/App.ts
+++ src/App.ts
@@ -7,9 +7,4 @@ app.use('/', (req, res) => {
 	res.send('Hello world	');
 })
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log("ENV");
-	console.log(process.env)
-	console.log(`PORT: ${process.env.port}`);
-	console.log(`Server is listening on port: ${PORT}`);
-})
+app.listen(PORT);
